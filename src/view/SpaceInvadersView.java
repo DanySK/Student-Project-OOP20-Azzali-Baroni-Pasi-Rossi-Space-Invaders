dbaba5997bfbf0b471f10972df11f74f1388c783
Alberto Rossi <albertoreds98@gmail.com>
2021-03-08 18:03:50 +0100

    Implementato campo di gioco al click play nel menù. inserendo sfondo di gioco e score

--- src/view/SpaceInvadersView.java
+++ src/view/SpaceInvadersView.java
@@ -4,4 +4,6 @@ public interface SpaceInvadersView {
 
 	void start();
 	
+	void setScore(int n);
+	
 }
--- src/view/SpaceInvadersViewImpl.java
+++ src/view/SpaceInvadersViewImpl.java
@@ -2,22 +2,30 @@ package view;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.net.URL;
+
 
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class SpaceInvadersViewImpl implements SpaceInvadersView{
 	
 	private static final String TITLE = "SpaceInvaders";
-	private static final double HEIGHT=854;
-	private static final double WIDTH = 480;
+	private static final double HEIGHT=480;
+	private static final double WIDTH = 854;
 	private static final double FONT_SIZE=18;
+	private final Label score = new Label();
 	private Stage secondaryStage;
 	private final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	private Pane root;
+	URL url = getClass().getClassLoader().getResource("BackgroundMenu.jpg");
 	
 	
 	public SpaceInvadersViewImpl(final Stage secondaryStage) {
@@ -30,14 +38,35 @@ public class SpaceInvadersViewImpl implements SpaceInvadersView{
 		this.secondaryStage.setTitle(TITLE);
 		this.secondaryStage.centerOnScreen();
 		//settare sfondo di gioco
-		this.root = new Pane();
-		final Scene scene = new Scene(this.root,screenSize.getWidth(),screenSize.getHeight());
-		final ImageView background = new ImageView(new Image(ClassLoader.getSystemResource("BackgroundMenu.jpg").toString()));
-		this.root.getChildren().add(background);
-		this.start();
-	   
+		this.setBackgroundGame(screenSize);
 	
 	}
+	private void setBackgroundGame(final Dimension sxreensize) {
+		this.root = new Pane();
+		final Scene scene = new Scene(this.root,screenSize.getWidth(),screenSize.getHeight());
+		final ImageView background = new ImageView(new Image(ClassLoader.getSystemResource("backgroundGame.png").toString()));
+		this.root.getChildren().add(background);
+		
+		this.score.setText("Score: 0");
+		this.score.setFont(new Font("Arial",this.root.getHeight() / FONT_SIZE));
+		this.score.setTextFill(Paint.valueOf(String.valueOf(Color.WHITE)));
+		this.root.getChildren().add(this.score);
+		
+		background.fitWidthProperty().bind(root.widthProperty());
+		background.fitHeightProperty().bind(root.heightProperty());
+		
+		//this.start();
+		
+		secondaryStage.setScene(scene);
+	   
+	}
+
+	@Override
+	public void setScore(final int score) {
+	  this.score.setText("Score: "+ score);
+		
+	}
+	
 	
 
 }
