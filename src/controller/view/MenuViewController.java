b9adcdfa88248710bf46fbda36868acf63404e05
Alberto Rossi <albertoreds98@gmail.com>
2021-03-08 10:54:42 +0100

    mergering

--- src/controller/view/MenuViewController.java
+++ src/controller/view/MenuViewController.java
@@ -1,32 +1,72 @@
 package controller.view;
 
-
 import java.io.IOException;
 
-
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 
 public class MenuViewController {
 
-	public void clickPlay(final ActionEvent event) {
-        final Stage secondaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        //new (primaryStage);
-	}
-/*@FXML
-	public void clickScore(final ActionEvent event) throws IOException{
-	
-	final Parent scoreBoard = FXMLLoader.load(ClassLoader.getSystemResource("layouts/scoreboard.fxml"));
-	final Scene scoreBoardScene = new Scene(scoreBoard);
-	final Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-	
-	window.setScene(scoreBoardScene);
-	window.show();
-}*/
+    @FXML
+    private Pane RootPane;
+
+    @FXML
+    private Button PlayButton;
+
+    @FXML
+    private Button ScoreButton;
+
+    @FXML
+    private Button HelpButton;
+
+    @FXML
+    private Button ExitButton;
+
+    @FXML
+    void playGame(ActionEvent event) {
+        System.out.println("GIOCA");
+    }
+
+    @FXML
+    void openScore(ActionEvent event) throws IOException {
+        Pane scorePane = FXMLLoader.load(getClass().getResource("/scoreBoard.fxml"));
+        RootPane.getChildren().setAll(scorePane);
+    }
+
+    @FXML
+    void openHelp(ActionEvent event) {
+        System.out.println("AIUTO");
+    }
+
+    @FXML
+    void closeGame(ActionEvent event) {
+        Platform.exit();
+    }
+
 
 }
+
+/*public class MenuViewController {
+
+    @FXML
+    void clickScore(ActionEvent event) throws IOException {
+    	final Parent scoreBoard = FXMLLoader.load(getClass().getResource("/scoreBoard.fxml"));
+    	final Scene scoreBoardScene = new Scene(scoreBoard);
+    	final Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    	
+    	window.setScene(scoreBoardScene);
+    	window.show();
+    }
+
+    @FXML
+    void clickPlay(ActionEvent event) {
+    	final Stage secondaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    	 new SpaceInvadersControllerImpl(secondaryStage);
+    }
+}*/
+
+
