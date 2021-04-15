06b6b23f17b6d6f29e02906c0e90bca3caacc019
Michele Pasi <michele.pasi@studio.unibo.it>
2021-04-15 11:30:37 +0200

    Creazione e Gestione Nemici

---
+++ src/model/EnemyImpl.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class EnemyImpl implements Enemy{
+	
+	private final List<Integer> listEnemyX;
+	private final List<Integer> listEnemyY;
+	private final int hit; 
+	
+	public EnemyImpl(final int hit) {
+		this.hit = hit;
+		listEnemyX = new LinkedList<>();
+		listEnemyY = new LinkedList<>();
+	}
+
+	@Override
+	public boolean tiedupX(final int n) {
+		for (final int x : listEnemyX) {
+			if (x == n || (x >= n - hit && x <= n + hit)) {
+				return true;
+			}
+		}
+		
+		return false;
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean tiedupY(final int n) {
+		for (final int x : listEnemyY) {
+			if (x == n || (x >= - hit && x <= n + hit)) {}
+			return true;
+		}
+		
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void deleteList() {
+		listEnemyX.clear();
+		listEnemyY.clear();
+		
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addNumber(boolean list, int n) {
+		if(list) {
+			listEnemyX.add(n);
+			}
+		else {
+			listEnemyY.add(n);
+		}
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	}
+	
+	
+
+
