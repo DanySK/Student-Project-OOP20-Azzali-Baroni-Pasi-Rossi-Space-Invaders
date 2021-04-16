439fca7001c6bedbc07b0e2e7c59b288504fe4f5
Alberto Rossi <albertoreds98@gmail.com>
2021-04-17 00:28:41 +0200

    inserisco main sparito

---
+++ src/application/Main.java
@@ -0,0 +1,33 @@
+package application;
+	
+import javax.swing.SwingUtilities;
+
+import controller.Controller;
+import controller.ControllerImpl;
+import view.View;
+import view.ViewImpl;
+
+
+
+public class Main {
+	
+	public static void main(final String[] args) {
+		//ImageLoader.getLoader().findImages();
+		//FontLoader.loadFont();
+		final Controller controller = new ControllerImpl();
+		final View view = new ViewImpl();
+		view.attach(controller);
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				view.start();
+				
+			}
+			
+		});
+		
+	}
+
+	
+}
