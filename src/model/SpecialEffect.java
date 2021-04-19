07fbc6dc76004476e2c2f9ac5ab609be822ce4d6
Michele Pasi <michele.pasi@studio.unibo.it>
2021-04-19 16:45:26 +0200

    Creazione Effetti Speciali

--- src/model/SpecialEffect.java
+++ src/model/SpecialEffect.java
@@ -1,5 +1,50 @@
 package model;
 
-public class SpecialEffect {
+import java.awt.Rectangle;
 
+import controller.ChronometerEntity;
+import utility.Pair;
+import view.EntityView;
+
+public class SpecialEffect extends ChronometerEntity {
+	
+	public enum SpecialEffectType {
+		
+		EXPLOSION (81 * EntityView.FRAME_IMAGE);
+		private final int lifetime;
+		
+		SpecialEffectType(final int lifetime){
+			this.lifetime = lifetime;
+		}
+		
+		public int getLifetime() {
+			return this.lifetime;
+		}
+		
+	}
+	
+	private final SpecialEffectType type;
+	
+	public SpecialEffect(final ID id, final Pair<Integer, Integer> position, final Rectangle hitbox, final SpecialEffectType type) {
+		super (position, 0, 0, id, type.getLifetime());
+		this.setHitbox(hitbox);
+		this.type = type;
+	}
+
+	@Override
+	public void update() {
+		this.tick();
+		if(this.isEnded()) {
+			this.setDead();
+		}
+	}
+	
+	@Override
+	public void collide (final Entity entity) {
+		
+	}
+	
+	public SpecialEffectType getType() {
+		return this.type;
+	}
 }
